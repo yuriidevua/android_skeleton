c45b681bb80da791af4755cae8fbecc62bf9dfeb
Yuri <lechenko.yura@gmail.com>
2023-11-01 15:03:19 +0200

    add inject router to presenter activity Main

--- presentation/src/main/java/com/sceleton/presentation/activity/MainPresenter.kt
+++ presentation/src/main/java/com/sceleton/presentation/activity/MainPresenter.kt
@@ -1,10 +1,12 @@
 package com.sceleton.presentation.activity
 
 import com.sceleton.portdomain.main.IMainUseCase
+import com.sceleton.presentation.router.IRouter
 import javax.inject.Inject
 
 class MainPresenter @Inject constructor(private val view : IMainView.View,
-                                        private val useCase : IMainUseCase
+                                        private val useCase : IMainUseCase,
+                                        private val router : IRouter
 )
     : IMainView.Presenter {
     override fun startView() {
